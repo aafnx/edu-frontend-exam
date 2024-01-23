4fa7d10836e63a0ff4572b0cc2c9cb48b2465e31
aa <68193417+aafnx@users.noreply.github.com>
2024-01-23 12:01:20 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push-f git@github.com:aafnx/edu-frontend-exam.git master:gh-pages
+git push -f git@github.com:aafnx/edu-frontend-exam.git master:gh-pages
 
 cd -
