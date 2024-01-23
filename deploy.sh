9877ef7812ba7c0958a03c96eb2baaa8aa5cf60d
aafnx <aa@fnx.red>
2024-01-23 11:56:59 +0300

    added script for deploy on gh pages

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+yarn build
+
+# переход в каталог сборки
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push-f git@github.com:aafnx/edu-frontend-exam.git master:gh-pages
+
+cd -
